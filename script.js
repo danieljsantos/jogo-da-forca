8275f149e563e75d4ccaf5d829946cb90ed62ce1
Daniel Santos <daan.js@gmail.com>
2019-02-15 12:07:33 -0200

    Jogo

---
+++ script.js
@@ -0,0 +1,45 @@
+const pMinutos = document.querySelector('.minutos');
+const pSegundos = document.querySelector('.segundos');
+let interval;
+
+const atualizarParagrafos = function (minutos, segundos) {
+  pMinutos.innerHTML = minutos;
+  pSegundos.innerHTML = segundos;
+}
+
+const atualizarTimer = function () {
+  let minuto = Number(pMinutos.innerHTML);
+  let segundo = Number(pSegundos.innerHTML);
+
+  if (segundo === 0) {
+    if (minuto === 0) {
+      clearInterval(interval);
+      // clearTimeout(valor)
+    }
+    else {
+      minuto --;
+      segundo = 59;
+    }
+  }
+  else {
+    segundo --;
+  }
+
+  atualizarParagrafos(minuto, segundo);
+}
+
+interval = setInterval(atualizarTimer, 1000);
+// setTimeout(atualizarTimer, 1000);
+
+
+function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+        ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
+        (charCode < 97 || charCode > 122)) {
+        alert("Enter letters only.");
+        return false;
+    }
+    return true;
+}
